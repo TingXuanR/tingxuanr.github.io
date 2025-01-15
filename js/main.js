e1b8218207e11e9f84bc4c6f5184b2e5eedfd8a0
R <riderpatch@outlook.com>
2025-01-15 17:55:09 +0800

    整理目录

--- js/main.js
+++ js/main.js
@@ -1588,13 +1588,3 @@ NexT.plugins.others.lawidget = function() {
     }, NexT.utils.fmtLaWidget());
   });
 }
-;
-/* Google translate plugin */
-NexT.plugins.others.translate = function() {
-  const element = '#gtranslate';
-  if (!NexT.utils.checkDOMExist(element)) return;
-  NexT.utils.lazyLoadComponent(element, function() { 
-    window.translateelement_styles='/css/google-translate.min.css'; 
-    NexT.utils.getScript('/js/third-party/google-translate.min.js');
-  });
-}
